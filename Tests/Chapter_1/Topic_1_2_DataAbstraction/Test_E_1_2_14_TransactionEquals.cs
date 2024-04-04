c75eee7b780daf3cdc2f186d503b3df5e89ab66d
Tymur Koshel <timurkoshel@gmail.com>
2024-04-04 15:57:06 -0700

    Exercise: 1.2.14 Transaction Equals (looks like I was kinda too fast to implement it before)

--- Tests/Chapter_1/Topic_1_2_DataAbstraction/Test_E_1_2_14_TransactionEquals.cs
+++ Tests/Chapter_1/Topic_1_2_DataAbstraction/Test_E_1_2_14_TransactionEquals.cs
@@ -4,16 +4,13 @@ using sedgewick_algorithms.Chapter_1.Topic_1_2_DataAbstraction;
 namespace Tests.Chapter_1.Topic_1_2_DataAbstraction
 {
     [TestFixture]
-    public class Test_E_1_2_13_Transaction
+    public class Test_E_1_2_14_TransactionEquals
     {
         [Test]
-        public void SmokeTest()
+        public void Smoke()
         {
             var transaction = new Transaction("Tymur", new Date(4, 4, 2024), 100);
-            
-            Assert.AreEqual("Tymur", transaction.Customer);
-            Assert.AreEqual(new Date(4, 4, 2024), transaction.Date);
-            Assert.AreEqual(100, transaction.Amount);
+            Assert.AreEqual(new Transaction("Tymur", new Date(4, 4, 2024), 100), transaction);
         }
     }
 }
