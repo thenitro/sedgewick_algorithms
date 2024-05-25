fc20ce05fc7fa88ec2c6ef05a84f80654bc99db8
Tymur Koshel <timurkoshel@gmail.com>
2024-05-25 07:16:56 -0700

    Exercise: 1.3.22 Remove After node in Linked List

---
+++ Tests/Chapter_1/Topic_1_3_BagsQueuesAndStacks/Test_E_1_3_22_RemoveAfter.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using sedgewick_algorithms.Chapter_1.Topic_1_3_BagsQueuesAndStacks;
+
+namespace Tests.Chapter_1.Topic_1_3_BagsQueuesAndStacks
+{
+    [TestFixture]
+    public class Test_E_1_3_22_RemoveAfter
+    {
+        [Test]
+        public void Smoke()
+        {
+            var node1 = new Node<int>(1);
+            var node2 = new Node<int>(2);
+            var node3 = new Node<int>(3);
+
+            node1.Next = node2;
+            node2.Next = node3;
+            
+            new E_1_3_22_RemoveAfter().Solution(node1);
+            Assert.AreEqual(3, node1.Next.Value);
+
+            node1.Next = node2;
+            node2.Next = node3;
+            
+            new E_1_3_22_RemoveAfter().Solution(node2);
+            Assert.AreEqual(2, node1.Next.Value);
+            Assert.Null(node2.Next);
+            
+            node1.Next = node2;
+            node2.Next = node3;
+            
+            new E_1_3_22_RemoveAfter().Solution(node3);
+        }
+    }
+}
---
+++ sedgewick_algorithms/Chapter_1/Topic_1_3_BagsQueuesAndStacks/E_1_3_22_RemoveAfter.cs
@@ -0,0 +1,12 @@
+namespace sedgewick_algorithms.Chapter_1.Topic_1_3_BagsQueuesAndStacks
+{
+    public class E_1_3_22_RemoveAfter
+    {
+        public void Solution(Node<int> current)
+        {
+            if (current == null) return;
+
+            current.Next = current.Next?.Next;
+        }
+    }
+}
